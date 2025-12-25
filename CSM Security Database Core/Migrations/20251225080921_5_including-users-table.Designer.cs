3caebf2d9681ca3ea002e34de56faa670f99d7c7
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2025-12-25 00:09:32 -0800

    added snapshot to include users in db model

--- CSM Security Database Core/Migrations/20251225080921_5_including-users-table.Designer.cs
+++ CSM Security Database Core/Migrations/20251225080921_5_including-users-table.Designer.cs
@@ -4,6 +4,7 @@ using CSM_Security_Database_Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CSM_Security.Migrations
 {
     [DbContext(typeof(SecurityDatabase))]
-    partial class DatabaseModelSnapshot : ModelSnapshot
+    [Migration("20251225080921_5_including-users-table")]
+    partial class _5_includinguserstable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
