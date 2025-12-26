7c9c3ed9a2218c994ce62e14b7f528c3facc748d
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2025-12-25 22:30:59 -0800

    added pactches to include methods

--- CSM Security Database Core/Depots/Abstractions/Interfaces/IUsersDepot.cs
+++ CSM Security Database Core/Depots/Abstractions/Interfaces/IUsersDepot.cs
@@ -9,4 +9,15 @@ namespace CSM_Security_Database_Core.Depots.Abstractions.Interfaces;
 /// </summary>
 public interface IUsersDepot
     : IDepot<User> {
+
+    /// <summary>
+    ///     Calculates the effective permits from the given <paramref name="id"/> as an <see cref="Account"/>.
+    /// </summary>
+    /// <param name="id">
+    ///     <see cref="IEntity.Id"/> pointer identifier for the <see cref="Account"/> to calculate its effective permits.
+    /// </param>
+    /// <returns>
+    ///     Effective <see cref="Permit"/> collection for the given <see cref="Account"/>.
+    /// </returns>
+    Task<Permit[]> GetPermits(long id);
 }
--- CSM Security Database Core/Depots/UsersDepot.cs
+++ CSM Security Database Core/Depots/UsersDepot.cs
@@ -1,10 +1,14 @@
-﻿using CSM_Database_Core.Depots.Abstractions.Bases;
+﻿using CSM_Database_Core.Core.Errors;
+using CSM_Database_Core.Depots.Abstractions.Bases;
+using CSM_Database_Core.Depots.Models;
 using CSM_Database_Core.Entities.Abstractions.Interfaces;
 
 using CSM_Foundation_Core.Abstractions.Interfaces;
 
 using CSM_Security_Database_Core.Depots.Abstractions.Interfaces;
 using CSM_Security_Database_Core.Entities;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace CSM_Security_Database_Core.Depots;
 
@@ -15,13 +19,67 @@ public class UsersDepot
     /// <summary>
     ///     Creates a new instance.
     /// </summary>
-    /// <param name="Database">
+    /// <param name="database">
     ///     Security database context.
     /// </param>
-    /// <param name="Disposer">
+    /// <param name="disposer">
     ///     Data disposr context manager.
     /// </param>
-    public UsersDepot(SecurityDatabase Database, IDisposer<IEntity>? Disposer)
-        : base(Database, Disposer) {
+    public UsersDepot(SecurityDatabase database, IDisposer<IEntity>? disposer)
+        : base(database, disposer) {
+    }
+
+    public async Task<Permit[]> GetPermits(long id) {
+        BatchOperationOutput<User> readOutput = await Read(
+                new QueryInput<User, FilterQueryInput<User>> {
+                    Parameters = new FilterQueryInput<User> {
+                        Behavior = FilteringBehaviors.First,
+                        Filter = (record) => record.Id == id,
+                    },
+                    PostProcessor = (query) => {
+                        return query
+                            .Include(a => a.Permits)
+                            .Include(a => a.Profiles)
+                                .ThenInclude(p => p.Permits);
+                    },
+                }
+            );
+
+        if (readOutput.Failed)
+            throw readOutput.Failures[0].Exception!;
+
+        if (readOutput.SuccessesCount <= 0)
+            throw new DepotError<User>(DepotErrorEvents.UNFOUND);
+
+        User User = readOutput.Successes[0];
+
+        List<Permit> effectivePermits = [];
+
+        bool VerifyEffective(Permit permit) {
+            return
+                permit.IsEnabled
+                && permit.Feature.IsEnabled
+                && permit.Action.IsEnabled
+                && !effectivePermits.Any(ePermit => ePermit.Id == permit.Id);
+        }
+
+
+        foreach (Profile profile in User.Profiles) {
+            foreach (Permit profilePermit in profile.Permits) {
+                if (!VerifyEffective(profilePermit))
+                    continue;
+
+                effectivePermits.Add(profilePermit);
+            }
+        }
+
+        foreach (Permit permit in User.Permits) {
+            if (!VerifyEffective(permit))
+                continue;
+
+            effectivePermits.Add(permit);
+        }
+
+        return [.. effectivePermits];
     }
 }
